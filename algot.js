b2c2e00171aee3bc43fab7ba6ceaeb5ea09077af
jerosas00567 <jeronimosalazar770@gmail.com>
2025-08-19 10:41:29 -0500

    Agrega algot.js e index.html al proyecto

---
+++ algot.js
@@ -0,0 +1,7 @@
+
+let compra1 = +prompt("ingrese el precio del producto:");
+let compra2 = +prompt("ingrese el precio del producto:");
+let compra3 = +prompt("ingrese el precio del producto:");
+let total = compra1+compra2+compra3;  
+alert=(" El total a pagar es:", suma);
+console=(" El total a pagar es:", suma);
