4e76e348edb0511a28d9419471caa83af626c9d8
hernan <caicedohernandario@gmail.com>
2025-09-01 22:59:18 -0500

    se agrega ejercicio 2

---
+++ ejercicio 2/algoritmo 2.js
@@ -0,0 +1,5 @@
+      let peso = prompt("ingrese su peso");
+        let altura = prompt("ingrese su altura");
+        let imc = peso/altura*2;
+    alert = ("su indice de masa corporal es ", imc);
+    console.log = ("su indice de masa corporañ es ", imc);
